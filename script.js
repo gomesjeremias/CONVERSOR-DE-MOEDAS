7ad775b3f799cda49431ffbe576f93e87f2066a3
gomesjeremias <gomesjeremias@gmail.com>
2024-08-30 21:03:01 -0300

    final of project , Add change image when click currency select

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select-change")
 
-function convertValues(){
-const inputCurrencyValue = document.querySelector(".input-currency").value
-const currencyValueToConvert = document.querySelector(".currency-value-to-convert")//valor em real
-const currencyValueConverted = document.querySelector(".currency-value")//Outras moedas
-const dolarToday = 5.2
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")//valor em real
+    const currencyValueConverted = document.querySelector(".currency-value")//Outras moedas
+    const dolarToday = 5.2
+    const euroToday = 6.2
 
-const convertValue = inputCurrencyValue / dolarToday
+    console.log(currencySelect.value)
 
-currencyValueToConvert.innerHTML = inputCurrencyValue
 
-console.log(convertValue)
+
+    if (currencySelect.value == "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+    if (currencySelect.value == "euro") {
+
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+
+}
+
+function changeCurrency() {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
+
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dolar americano"
+        currencyImage.src = "./assets/dolar.png"
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assets/euro.png"
+    }
+    
 }
 
 
+currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
